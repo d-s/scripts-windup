e29f6f9808a3b86ced4e5388f90f035ca04505c6
Dan Smith <dansmit@redhat.com>
2016-05-23 17:51:19 -0400

    script to sync local windup repos

---
+++ sync-local.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Syncs local git repos for core Windup projects with latest upstream master.
+#
+# Usage [from parent directory of projects to sync]: sync-local.sh
+
+readonly UPSTREAM_REMOTE_NAME="upstream"
+readonly PROJECTS_ORDERED="windup windup-rulesets windup-distribution"
+
+for project in $PROJECTS_ORDERED; do
+  cd $project
+  git checkout master
+  git pull upstream master
+  cd ..
+done
+
